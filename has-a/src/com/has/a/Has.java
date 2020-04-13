39b5beba6e31aef8ed46d0aeaff46f642dc04aab
pradeep-kumar-maurya <pk454649@gmail.com>
2020-04-13 12:22:39 +0530

    Class Has has been changed

--- has-a/src/com/has/a/Has.java
+++ has-a/src/com/has/a/Has.java
@@ -24,9 +24,14 @@ class C extends B{
 }
 
 public class Has {
+	void display3() {
+		System.out.println("main class");
+	}
 	public static void main(String[] args) {
 		A a = new A();
 		C c = new C(a);
+		Has has = new Has();
+		has.display3();
 		c.display1();
 		c.display2();
 	}
